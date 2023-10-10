22d5290c84c666f24707461be8fb8019edb75bfb
shin-hama <htysivista@gmail.com>
2023-10-11 06:25:52 +0900

    FIX: Add env param in turbo.json

--- apps/browser/src/lib/config.ts
+++ apps/browser/src/lib/config.ts
@@ -1,5 +1,5 @@
 if (!process.env.PLASMO_PUBLIC_APP_HOST) {
-  throw new Error("PLASMO_PUBLIC_API_HOST is not set");
+  throw new Error("PLASMO_PUBLIC_APP_HOST is not set");
 }
 
 export const APP_HOST = process.env.PLASMO_PUBLIC_APP_HOST;
