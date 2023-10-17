ec8cc34bbd4b04edb426e64a548b875e844eb805
shin-hama <htysivista@gmail.com>
2023-10-17 09:30:56 +0900

    FIX: refs #20 Labels を LocalStorage に保存するように変更

--- apps/browser/src/background/messages/labels/list.ts
+++ apps/browser/src/background/messages/labels/list.ts
@@ -1,18 +1,36 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging";
+import { Storage } from "@plasmohq/storage";
 
 import type { Label } from "@whl/db";
 
 import { APP_HOST } from "~/lib/config";
 
+const LABELS_KEY = "labels";
+
 const handler: PlasmoMessaging.MessageHandler<undefined, Label[]> = async (
   req,
   res,
 ) => {
+  const storage = new Storage({
+    area: "local",
+  });
+
+  // labels が保存済みであればそれを使う
+  // データの更新は、アプリ上で更新したときに拡張機能側で検知する
+  let labels = await storage.getItem<Label[]>(LABELS_KEY);
+  if (labels) {
+    res.send(labels);
+    return;
+  }
+
   const result = await fetch(`${APP_HOST}/api/labels`, {
     method: "GET",
   });
 
-  res.send((await result.json()) as Label[]);
+  labels = (await result.json()) as Label[];
+  await storage.setItem(LABELS_KEY, labels);
+
+  res.send(labels);
 };
 
 export default handler;
