7676a39606293df3301698bc73429bac3c5b5674
shin-hama <htysivista@gmail.com>
2023-10-17 11:22:08 +0900

    FEAT: Popup 画面のレイアウトを調整

---
+++ apps/browser/src/popup/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="whl-flex whl-w-full whl-flex-row whl-bg-primary whl-px-4 whl-py-2 whl-text-primary-foreground">
+      <h1 className="whl-font-mono whl-text-4xl whl-font-bold">Highlighter</h1>
+    </div>
+  );
+};
+
+export default Header;
--- apps/browser/src/popup/index.tsx
+++ apps/browser/src/popup/index.tsx
@@ -3,6 +3,7 @@ import "@whl/ui/app/globals.css";
 import { Button } from "@whl/ui/components/ui/button";
 
 import { useSession } from "~/hooks/useSession";
+import Header from "./components/Header";
 import Highlights from "./components/Highlights";
 
 function IndexPopup() {
@@ -13,27 +14,33 @@ function IndexPopup() {
   }
 
   return (
-    <div className="whl-flex whl-h-64 whl-w-80 whl-items-center whl-justify-center">
+    <div className="whl-h-64 whl-w-80">
+      <Header />
       {status === "authenticated" ? (
-        <>
-          <div>latest highlights</div>
+        <div className="whl-w-full whl-gap-2 whl-pt-2">
+          <div className="whl-px-4">
+            <div className="whl-font-bold">Quick Feeds</div>
+          </div>
           <Highlights />
-        </>
+          <Button className="whl-w-full">Show More</Button>
+        </div>
       ) : (
-        <div className="whl-flex whl-flex-col whl-items-center whl-space-y-8 whl-px-12">
-          <p>
-            If record the highlight, you need sign in or create new account.
-          </p>
-          <div>
-            <Button asChild>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="http://localhost:3000/api/auth/signin"
-              >
-                Sign in
-              </a>
-            </Button>
+        <div className="whl-flex whl-flex-col whl-items-center whl-justify-center">
+          <div className="whl-flex whl-flex-col whl-items-center whl-space-y-8 whl-px-12">
+            <p>
+              If record the highlight, you need sign in or create new account.
+            </p>
+            <div>
+              <Button asChild>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="http://localhost:3000/api/auth/signin"
+                >
+                  Sign in
+                </a>
+              </Button>
+            </div>
           </div>
         </div>
       )}
