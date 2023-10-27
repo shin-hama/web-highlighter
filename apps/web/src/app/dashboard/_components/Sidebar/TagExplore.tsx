7223c0cd1fa697c1c2dc0ee86dc34da28b837fe7
shin-hama <htysivista@gmail.com>
2023-10-27 10:46:07 +0900

    FEAT: tag を検索できるようにした

--- apps/web/src/app/dashboard/_components/Sidebar/TagExplore.tsx
+++ apps/web/src/app/dashboard/_components/Sidebar/TagExplore.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import React, { useState } from "react";
 import { SearchIcon } from "lucide-react";
 
 import { Input } from "@whl/ui/components/ui/input";
@@ -5,19 +8,36 @@ import { Toggle } from "@whl/ui/components/ui/toggle";
 
 // test data of tag list
 const tags = [
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
-  "test",
+  "apple",
+  "banana",
+  "car",
+  "dog",
+  "elephant",
+  "flower",
+  "guitar",
+  "house",
+  "ice cream",
+  "jacket",
+  "kangaroo",
+  "lion",
+  "mountain",
+  "notebook",
+  "ocean",
+  "piano",
+  "queen",
+  "river",
+  "sun",
+  "tree",
+  "umbrella",
+  "violin",
+  "waterfall",
+  "xylophone",
+  "yacht",
+  "zebra",
 ];
 
 const TagExplore = () => {
+  const [query, setQuery] = useState("");
   return (
     <div className="whl-h-full whl-w-48 whl-bg-primary whl-px-4 whl-pb-4 whl-pt-2 whl-text-primary-foreground">
       <div className="whl-h-full">
@@ -31,18 +51,22 @@ const TagExplore = () => {
                 icon={<SearchIcon size={16} />}
                 placeholder="Search tags..."
                 className="whl-border-muted-foreground whl-bg-primary"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
               />
             </div>
             <div className="whl-flex whl-flex-col whl-gap-1">
-              {tags.map((tag, i) => (
-                <Toggle
-                  key={`${tag}-${i}`}
-                  size="xs"
-                  className="whl-justify-start"
-                >
-                  # {tag}
-                </Toggle>
-              ))}
+              {tags
+                .filter((tag) => tag.includes(query))
+                .map((tag, i) => (
+                  <Toggle
+                    key={`${tag}-${i}`}
+                    size="xs"
+                    className="whl-justify-start"
+                  >
+                    # {tag}
+                  </Toggle>
+                ))}
             </div>
           </div>
         </div>
