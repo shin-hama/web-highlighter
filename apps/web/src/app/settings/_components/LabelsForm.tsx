5482fca1eb9c2cb8c96ddae32dbbc2be2d202cea
shin-hama <htysivista@gmail.com>
2023-11-01 10:42:57 +0900

    WIP: Add save and cancel button on the labels form

--- apps/web/src/app/settings/_components/LabelsForm.tsx
+++ apps/web/src/app/settings/_components/LabelsForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@ui/components/ui/button";
 import { z } from "zod";
 
 import type { Label } from "@whl/db";
@@ -14,20 +15,34 @@ import {
 } from "@whl/ui/components/ui/form";
 import { Input } from "@whl/ui/components/ui/input";
 
-const labelsFormSchema = z.object({
-  labels: z.array(z.string()),
+const LabelsFormSchema = z.object({
+  labels: z.array(
+    z.object({
+      name: z.string(),
+      color: z.string(),
+      id: z.string(),
+    }),
+  ),
 });
+type LabelsForm = z.infer<typeof LabelsFormSchema>;
 
 interface Props {
   labels: Label[];
 }
 const LabelsForm = ({ labels }: Props) => {
-  const form = useForm<z.infer<typeof labelsFormSchema>>({
-    resolver: zodResolver(labelsFormSchema),
-    defaultValues: { labels: labels.map((label) => label.name ?? "") },
+  const form = useForm<LabelsForm>({
+    resolver: zodResolver(LabelsFormSchema),
+    defaultValues: {
+      labels: labels.map((label) => ({
+        ...label,
+        name: label.name ?? "",
+      })),
+    },
   });
 
-  const handleSubmit = (values: z.infer<typeof labelsFormSchema>) => {
+  const { formState } = form;
+
+  const handleSubmit = (values: LabelsForm) => {
     console.log(values);
   };
 
@@ -41,7 +56,7 @@ const LabelsForm = ({ labels }: Props) => {
           <FormField
             key={label.id}
             control={form.control}
-            name={`labels.${i}`}
+            name={`labels.${i}.name`}
             render={({ field }) => (
               <FormItem>
                 <div className="whl-flex whl-items-center whl-gap-2">
@@ -58,7 +73,14 @@ const LabelsForm = ({ labels }: Props) => {
             )}
           />
         ))}
-        <button type="submit">Submit</button>
+        {formState.isDirty && (
+          <div className="whl-flex whl-w-full whl-justify-end whl-gap-2">
+            <Button type="reset" variant="outline" onClick={() => form.reset()}>
+              Cancel
+            </Button>
+            <Button type="submit">Save</Button>
+          </div>
+        )}
       </form>
     </Form>
   );
--- apps/web/src/app/settings/page.tsx
+++ apps/web/src/app/settings/page.tsx
@@ -14,7 +14,7 @@ const Settings = async () => {
   const labels = await getLabels();
 
   return (
-    <div className="whl-container whl-flex whl-flex-col whl-gap-4">
+    <div className="whl-container whl-flex whl-flex-col whl-gap-4 whl-py-8">
       <Card>
         <CardHeader>
           <CardTitle>Labels</CardTitle>
