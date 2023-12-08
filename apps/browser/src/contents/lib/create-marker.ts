2d46969a01a347b451c27fc3cd07ed1f8919e51a
shin-hama <htysivista@gmail.com>
2023-12-08 10:33:54 +0900

    FIX: refs #64 MARKER のいちが正しく復元できていない問題を修正

--- apps/browser/src/contents/lib/create-marker.ts
+++ apps/browser/src/contents/lib/create-marker.ts
@@ -1,5 +1,10 @@
 import { sendToContentScript } from "@plasmohq/messaging";
 
+import {
+  getTextNodesInOneElement,
+  getTextNodesInRange,
+} from "./get-text-nodes";
+
 export function createMarker(
   node: Node,
   color: string,
@@ -45,4 +50,76 @@ export function createMarker(
   });
 
   range.surroundContents(elm);
+}
+
+function processTextNodes(
+  textNodes: Node[],
+  color: string,
+  startOffset: number,
+  endOffset: number,
+) {
+  textNodes.reduce((count, textNode) => {
+    const tempCount = count + (textNode.textContent?.length ?? 0);
+    if (tempCount > startOffset && count < endOffset) {
+      createMarker(textNode, color, {
+        start:
+          count < startOffset
+            ? (textNode.textContent?.length ?? 0) - (tempCount - startOffset)
+            : undefined,
+        end: tempCount >= endOffset ? endOffset - count : undefined,
+      });
+    }
+    return tempCount;
+  }, 0);
+}
+
+function processTextNodesInOneElement(
+  element: Element,
+  color: string,
+  startOffset: number,
+  endOffset: number,
+) {
+  const textNodes = getTextNodesInOneElement(element);
+
+  processTextNodes(textNodes, color, startOffset, endOffset);
+}
+
+function processTextNodesBetweenTwoElements(
+  start: Element,
+  end: Element,
+  color: string,
+  startOffset: number,
+  endOffset: number,
+) {
+  const otherRange = document.createRange();
+  otherRange.setStart(start, start?.childNodes.length);
+  otherRange.setEnd(end, 0);
+
+  const startNodes = getTextNodesInOneElement(start);
+  const otherTextNodes = getTextNodesInRange(otherRange);
+  const endNodes = getTextNodesInOneElement(end);
+
+  processTextNodes(startNodes, color, startOffset, Infinity);
+  processTextNodes(otherTextNodes, color, 0, Infinity);
+  processTextNodes(endNodes, color, 0, endOffset);
+}
+
+export function createMarkers(
+  startElement: Element,
+  endElement: Element,
+  color: string,
+  startOffset: number,
+  endOffset: number,
+) {
+  if (startElement === endElement) {
+    processTextNodesInOneElement(startElement, color, startOffset, endOffset);
+  } else {
+    processTextNodesBetweenTwoElements(
+      startElement,
+      endElement,
+      color,
+      startOffset,
+      endOffset,
+    );
+  }
 }
