d93f093e43d168b48a40f4e12f1965ae813877fe
shin-hama <htysivista@gmail.com>
2023-12-12 09:45:20 +0900

    FEAT: Dashboard 上に TextFragment のリンクを開くボタンを追加

--- apps/web/src/app/dashboard/_components/Highlight/Actions.tsx
+++ apps/web/src/app/dashboard/_components/Highlight/Actions.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Trash2 } from "lucide-react";
+import { ExternalLink, Trash2 } from "lucide-react";
 import { useSWRConfig } from "swr";
 
 import { Button } from "@whl/ui/components/ui/button";
@@ -8,8 +8,9 @@ import { Button } from "@whl/ui/components/ui/button";
 interface Props {
   id: string;
   pageId: string;
+  url: string;
 }
-export const Actions = ({ id, pageId }: Props) => {
+export const Actions = ({ id, pageId, url }: Props) => {
   const { mutate } = useSWRConfig();
   const handleRemove = async () => {
     await fetch(`/api/highlights/${id}`, {
@@ -20,8 +21,21 @@ export const Actions = ({ id, pageId }: Props) => {
   };
 
   return (
-    <Button size="icon_sm" variant="ghost" onClick={handleRemove}>
-      <Trash2 size="20" />
-    </Button>
+    <div className="whl-flex whl-border-spacing-2 whl-flex-row">
+      {url && (
+        <Button
+          variant="ghost"
+          size="icon_sm"
+          onClick={() => {
+            window.open(url, "_blank");
+          }}
+        >
+          <ExternalLink />
+        </Button>
+      )}
+      <Button size="icon_sm" variant="ghost" onClick={handleRemove}>
+        <Trash2 size="20" />
+      </Button>
+    </div>
   );
 };
