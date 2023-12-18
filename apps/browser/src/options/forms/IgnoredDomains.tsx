80bd6ed2b1289670fc7ac514a2b97e955afa276d
shin-hama <htysivista@gmail.com>
2023-12-18 09:39:27 +0900

    FEAT: IgnoredDomain を入力するオプションページを作成

---
+++ apps/browser/src/options/forms/IgnoredDomains.tsx
@@ -0,0 +1,12 @@
+import { Textarea } from "@ui/components/ui/textarea";
+
+const IgnoredDomains = () => {
+  return (
+    <div className="whl-space-y-2">
+      <h2>Ignored Domains</h2>
+      <Textarea />
+    </div>
+  );
+};
+
+export default IgnoredDomains;
