2fd173af5188295912dc44116352db6fdd983b85
shin-hama <htysivista@gmail.com>
2023-12-25 09:29:25 +0900

    FEAT: URL を Clipboard にコピーするボタンを追加

--- apps/web/src/app/dashboard/_components/Highlight/Actions.tsx
+++ apps/web/src/app/dashboard/_components/Highlight/Actions.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import Link from "next/link";
-import { ExternalLink, Trash2 } from "lucide-react";
+import { ClipboardCopyIcon, ExternalLink, Trash2 } from "lucide-react";
 import { useSWRConfig } from "swr";
 
 import { Button } from "@whl/ui/components/ui/button";
 
+const ActionIconSize = 20;
 interface Props {
   id: string;
   pageId: string;
@@ -26,14 +27,25 @@ export const Actions = ({ id, pageId, url }: Props) => {
   return (
     <div className="whl-flex whl-border-spacing-2 whl-flex-row">
       {url && (
-        <Button variant="ghost" size="icon_sm" asChild>
-          <Link href={url} target="_blank" rel="noopener noreferrer">
-            <ExternalLink />
-          </Link>
-        </Button>
+        <>
+          <Button
+            variant="ghost"
+            size="icon_sm"
+            onClick={async () => {
+              await navigator.clipboard.writeText(url);
+            }}
+          >
+            <ClipboardCopyIcon size={ActionIconSize} />
+          </Button>
+          <Button variant="ghost" size="icon_sm" asChild>
+            <Link href={url} target="_blank" rel="noopener noreferrer">
+              <ExternalLink size={ActionIconSize} />
+            </Link>
+          </Button>
+        </>
       )}
       <Button size="icon_sm" variant="ghost" onClick={handleRemove}>
-        <Trash2 size="20" />
+        <Trash2 size={ActionIconSize} />
       </Button>
     </div>
   );
