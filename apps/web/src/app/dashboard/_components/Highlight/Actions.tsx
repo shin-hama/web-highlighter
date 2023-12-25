178a88dc0e0020d788a6df0e25a319ebba85631c
shin-hama <htysivista@gmail.com>
2023-12-25 10:17:17 +0900

    FEAT: Highlight Actions に Tooltip を追加

--- apps/web/src/app/dashboard/_components/Highlight/Actions.tsx
+++ apps/web/src/app/dashboard/_components/Highlight/Actions.tsx
@@ -1,10 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import Link from "next/link";
 import { ClipboardCopyIcon, ExternalLink, Trash2 } from "lucide-react";
 import { useSWRConfig } from "swr";
 
 import { Button } from "@whl/ui/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@whl/ui/components/ui/tooltip";
 
 const ActionIconSize = 20;
 interface Props {
@@ -22,31 +29,63 @@ export const Actions = ({ id, pageId, url }: Props) => {
     void mutate(`/api/highlights?pageId=${pageId}`);
   };
 
-  console.log("Actions", id, pageId, url);
+  const [clipboardTooltip, setClipboardTooltip] = useState("Copy to clipboard");
+  const [openClipboardTooltip, setOpenClipboardTooltip] = useState(false);
 
   return (
-    <div className="whl-flex whl-border-spacing-2 whl-flex-row">
-      {url && (
-        <>
-          <Button
-            variant="ghost"
-            size="icon_sm"
-            onClick={async () => {
-              await navigator.clipboard.writeText(url);
-            }}
-          >
-            <ClipboardCopyIcon size={ActionIconSize} />
-          </Button>
-          <Button variant="ghost" size="icon_sm" asChild>
-            <Link href={url} target="_blank" rel="noopener noreferrer">
-              <ExternalLink size={ActionIconSize} />
-            </Link>
-          </Button>
-        </>
-      )}
-      <Button size="icon_sm" variant="ghost" onClick={handleRemove}>
-        <Trash2 size={ActionIconSize} />
-      </Button>
-    </div>
+    <TooltipProvider delayDuration={300}>
+      <div className="whl-flex whl-border-spacing-2 whl-flex-row">
+        {url && (
+          <>
+            <Tooltip
+              open={openClipboardTooltip}
+              onOpenChange={(open) => {
+                setOpenClipboardTooltip(open);
+                if (open) {
+                  setClipboardTooltip("Copy link to highlight");
+                }
+              }}
+            >
+              <TooltipTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon_sm"
+                  onClick={async () => {
+                    await navigator.clipboard.writeText(url);
+                    setClipboardTooltip("Copied!");
+                    setOpenClipboardTooltip(true);
+                  }}
+                >
+                  <ClipboardCopyIcon size={ActionIconSize} />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent className="whl-text-xs">
+                {clipboardTooltip}
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button variant="ghost" size="icon_sm" asChild>
+                  <Link href={url} target="_blank" rel="noopener noreferrer">
+                    <ExternalLink size={ActionIconSize} />
+                  </Link>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent className="whl-text-xs">
+                Open highlight text
+              </TooltipContent>
+            </Tooltip>
+          </>
+        )}
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button size="icon_sm" variant="ghost" onClick={handleRemove}>
+              <Trash2 size={ActionIconSize} />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent className="whl-text-xs">Remove</TooltipContent>
+        </Tooltip>
+      </div>
+    </TooltipProvider>
   );
 };
