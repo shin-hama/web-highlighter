c9a77df73c32ee0fd7fe133c210a212defc5dcec
shin-hama <htysivista@gmail.com>
2024-02-01 09:51:02 +0900

    FEAT: tag の追加と削除を Editor 上でできるようにした

--- apps/web/src/app/dashboard/_components/Highlight/Editor.tsx
+++ apps/web/src/app/dashboard/_components/Highlight/Editor.tsx
@@ -1,4 +1,10 @@
 import { Button } from "@ui/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@ui/components/ui/popover";
+import { ScrollArea } from "@ui/components/ui/scroll-area";
 import { PlusIcon } from "lucide-react";
 
 import type { HighlightWithLabelAndPageAndTag } from "@whl/common-types";
@@ -6,6 +12,7 @@ import { Card } from "@whl/ui/components/ui/card";
 import { Textarea } from "@whl/ui/components/ui/textarea";
 
 import { Actions } from "./Actions";
+import AddTagsForm from "./AddTagsForm";
 import HighlightContent from "./HighlightContent";
 import { useHighlight } from "./hooks/useHighlight";
 import QuoteSource from "./ReferencedFooter";
@@ -23,35 +30,50 @@ const Editor = (props: HighlightWithLabelAndPageAndTag) => {
   const { content, HighlightOnTag, page } = highlight;
   return (
     <div
-      className=""
+      className="whl-flex whl-max-h-[80vh] whl-flex-col whl-overflow-hidden"
       style={{
         backgroundColor: highlight.label.color,
       }}
     >
-      <div>
-        <div className="whl-p-4">
-          <HighlightContent content={content} />
-        </div>
+      <div className="whl-p-4">
+        <HighlightContent content={content} />
+      </div>
+      <ScrollArea className="whl-flex-1">
         <div className="whl-px-4 whl-py-2">
-          <Textarea placeholder="Enter a note..." className="" />
+          <Textarea placeholder="Enter a note..." className="whl-h-[30vh]" />
         </div>
         <div className="whl-flex whl-flex-row whl-items-center whl-gap-1 whl-px-4 whl-py-2">
           {HighlightOnTag.map(({ tag }) => (
             <TagBadge key={tag.id} tag={tag} onRemoved={handleRemoveTag} />
           ))}
-          <Button
-            size="icon"
-            className="whl-aspect-square whl-h-5 whl-w-5 whl-rounded-full"
-          >
-            <PlusIcon size={12} />
-          </Button>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                size="icon"
+                className="whl-aspect-square whl-h-5 whl-w-5 whl-rounded-full"
+              >
+                <PlusIcon size={12} />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent
+              avoidCollisions={false}
+              side="bottom"
+              align="start"
+              className="whl-w-auto whl-px-0 whl-py-1"
+            >
+              <AddTagsForm
+                addedTags={HighlightOnTag.map((relation) => relation.tag)}
+                highlightId={highlight.id}
+              />
+            </PopoverContent>
+          </Popover>
         </div>
         <div className="whl-px-4 whl-py-2">
           <Card>
             <QuoteSource {...page} />
           </Card>
         </div>
-      </div>
+      </ScrollArea>
       <div className="whl-px-2 whl-py-2">
         <Actions {...highlight} />
       </div>
